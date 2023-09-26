71f4859e3052e319caa4ec9245b35269e18c5a7c
VladyslavKosarenko <131978971+VladyslavKosarenko@users.noreply.github.com>
2023-09-26 03:04:10 +0300

    finish

--- src/cat-api.js
+++ src/cat-api.js
@@ -10,11 +10,32 @@ export function fetchBreeds() {
       return response.json();
     })
     .then(result => {
-      console.log(result);
+      ;
       return result;
     })
     .catch(error => {
       console.error("Помилка отримання порід:", error);
+      throw error;
+    });
+}
+
+export function fetchCatByBreed(breedId) {
+  const urlCat = `https://api.thecatapi.com/v1/images/search?breed_ids=${breedId}`;
+  const apiKey = "live_LK29Yyc5d6HpqrbOdMOoKlvDw6B1nloufHgqastPSHYeMSNxPI5Cb6DASn28UB4b";
+  return fetch(urlCat, {
+    method: 'GET',
+    headers: {
+      'x-api-key': apiKey
+    }
+  })
+    .then(response => {
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+      return response.json();
+    })
+    .catch(error => {
+      console.error("Помилка отримання даних про котів:", error);
       throw error;
     });
 }
--- src/index.js
+++ src/index.js
@@ -2,15 +2,26 @@
 
 // axios.defaults.headers.common["x-api-key"] = "live_LK29Yyc5d6HpqrbOdMOoKlvDw6B1nloufHgqastPSHYeMSNxPI5Cb6DASn28UB4b";
 import { fetchBreeds } from "./cat-api";
+import {fetchCatByBreed} from "./cat-api";
 
 const refs = {
     select: document.querySelector('.breed-select'),
-    div: document.querySelector('.cat-info'),
+  div: document.querySelector('.cat-info'),
+  loader: document.querySelector('.loader'),
+  error: document.querySelector('.error'),
+}
+hideElement(refs.error);
+hideElement(refs.loader)
+function showElement(element) {
+  element.style.display = 'block';
 }
 
-
+function hideElement(element) {
+  element.style.display = 'none';
+}
 fetchBreeds()
   .then(result => {
+    
     const markup = result.map(({ id, name }) => {
       return `<option value = "${id}">${name}</option>}`;
     }).join('');
@@ -20,3 +31,32 @@ fetchBreeds()
   .catch(error => {
     console.error("Помилка обробки даних:", error);
   });
+
+refs.select.addEventListener("change", () => {
+  const selectedBreedId = refs.select.value;
+    hideElement(refs.div); 
+    showElement(refs.loader);
+    hideElement(refs.error)
+    fetchCatByBreed(selectedBreedId)
+      .then(catData => {
+        const catImage = catData[0].url;
+        const catName = catData[0].breeds[0].name;
+        const catDescription = catData[0].breeds[0].description;
+        const catTemperament = catData[0].breeds[0].temperament;
+        
+        const catInfoHTML = `
+          <img src="${catImage}" width='700' height='500' alt="${catName}">
+          <h2>${catName}</h2>
+          <p>${catDescription}</p>
+          <p>Temperament: ${catTemperament}</p>`;
+        refs.div.innerHTML = catInfoHTML;
+        hideElement(refs.loader); 
+        showElement(refs.div); 
+      })
+      .catch(error => {
+        console.log(error);
+        showElement(refs.error);
+        hideElement(refs.loader);
+      });
+      
+});
